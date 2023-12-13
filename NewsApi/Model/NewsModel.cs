5a11cfa5053315141598180fce5ded94a730b438
PardeepRsystems <153589101+PardeepRsystems@users.noreply.github.com>
2023-12-13 17:40:17 +0530

    Delete NewsApi directory

--- NewsApi/Model/NewsModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NewsApi.Model
-{
-    public class NewsModel
-    {
-        public int Id { get; set; }
-        public string? Title { get; set; }
-        public string? Url { get; set; }
-    }
-}
